5ac959bc19cbe25786bc86f63328c844b544e574
ARedikultseva <anredikultseva@gmail.com>
2023-10-18 20:07:57 +0500

    Task1

---
+++ Tyuiu.RedikultsevaAA.Sprint3.Task1.V17.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.RedikultsevaAA.Sprint3.Task1.V17.Lib
+{
+    public class DataService : ISprint3Task1V17
+    {
+        public double GetMultiplySeries(int value, int startValue, int stopValue)
+        {
+            double multScore = 1;
+
+            while (startValue <= stopValue)
+            {
+                multScore *= Math.Pow((2 / Math.Cos(value)), startValue);
+                startValue++;
+            }
+
+            return Math.Round(multScore, 3);
+        }
+    }
+}
