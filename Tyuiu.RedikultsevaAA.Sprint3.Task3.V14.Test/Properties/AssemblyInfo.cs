e0f60250cb3bc405ae2e35e97ddc14b247e3f320
ARedikultseva <anredikultseva@gmail.com>
2023-10-21 17:27:35 +0500

    Task3

---
+++ Tyuiu.RedikultsevaAA.Sprint3.Task3.V14.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.RedikultsevaAA.Sprint3.Task3.V14.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.RedikultsevaAA.Sprint3.Task3.V14.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4ae4ae98-3ce7-482b-a46c-11b10d9834e3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.RedikultsevaAA.Sprint3.Task3.V14/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.RedikultsevaAA.Sprint3.Task3.V14.Lib;
+
+namespace Tyuiu.RedikultsevaAA.Sprint3.Task3.V14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнила: Редикульцева А.А. | АСОиУб 23-1";
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* Спринт #3                                                                  *");
+            Console.WriteLine("* Тема: Оператор цикла foreach                                               *");
+            Console.WriteLine("* Задание #3                                                                 *");
+            Console.WriteLine("* Вариант #14                                                                *");
+            Console.WriteLine("* Выполнила: Редикульцева А.А. | АСОиУб 23-1                                 *");
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                   *");
+            Console.WriteLine("* Используя цикл foreach заменить буквы d  на ’ в строке: dgt ghedd bed      *");
+            Console.WriteLine("*                                                                            *");
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                           *");
+            Console.WriteLine("******************************************************************************");
+
+            string value = "dgt ghedd bed";
+            char replacebale = 'd';
+            char replacement = '’';
+
+            Console.WriteLine("Исходная строка = " + value);
+            Console.WriteLine("Заменяемый символ = " + replacebale);
+            Console.WriteLine("Символ, на который заменяют = " + replacement);
+
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                 *");
+            Console.WriteLine("******************************************************************************");
+
+            Console.WriteLine("Получившаяся строка = " + ds.ReplaceCharInString(value, replacebale, replacement));
+            Console.ReadKey();
+        }
+    }
+}
