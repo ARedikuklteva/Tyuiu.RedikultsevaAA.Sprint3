7bdc1a18f56e6718f95d065bf7653254314d3755
ARedikultseva <anredikultseva@gmail.com>
2023-10-21 16:58:40 +0500

    Task2

---
+++ Tyuiu.RedikultsevaAA.Sprint3.Task2.V19.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.RedikultsevaAA.Sprint3.Task2.V19.Lib;
+
+namespace Tyuiu.RedikultsevaAA.Sprint3.Task2.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSeries()
+        {
+            DataService ds = new DataService();
+
+            int startValue = 1;
+            int stopValue = 10;
+
+            double res = -0.709;
+            double wait = ds.GetSumSeries(startValue, stopValue);
+
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.RedikultsevaAA.Sprint3.Task2.V19.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.RedikultsevaAA.Sprint3.Task2.V19.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.RedikultsevaAA.Sprint3.Task2.V19.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0c773a9c-0390-40cf-889f-8ae74d7acb20")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
