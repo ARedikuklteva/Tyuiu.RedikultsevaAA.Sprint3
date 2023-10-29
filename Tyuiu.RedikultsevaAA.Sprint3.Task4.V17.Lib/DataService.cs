57d3eb9bf1250310faef07a9e149f836f5a17921
ARedikultseva <anredikultseva@gmail.com>
2023-10-29 14:49:15 +0500

    Task4

---
+++ Tyuiu.RedikultsevaAA.Sprint3.Task4.V17.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+// На отрезке, где x принимает значения от -5 до 5, вычислить значение функции y=cos(x)/sin(x)
+// При х = 0 прервать цикл. Полученные значения перемножать.
+
+namespace Tyuiu.RedikultsevaAA.Sprint3.Task4.V17.Lib
+{
+    public class DataService : ISprint3Task4V17
+    {
+        public double Calculate(int startValue, int stopValue)
+        {
+            double res = 1;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                if (x == 0)
+                    break;
+
+                res *= (Math.Cos(x) / Math.Sin(x));
+            }
+
+            return Math.Round(res,3);
+        }
+    }
+}
---
+++ Tyuiu.RedikultsevaAA.Sprint3.Task4.V17.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.RedikultsevaAA.Sprint3.Task4.V17.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.RedikultsevaAA.Sprint3.Task4.V17.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("853b1bf9-9053-461e-aa96-1765d1700e77")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
